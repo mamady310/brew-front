308481e872b3a39f37d2e2ec192214ee4516d8e6
Mamady Sillah <supurpisces310@msn.com>
2019-12-30 00:03:43 -0500

    added Home.js and edited App.js

--- brew-front/src/App.js
+++ brew-front/src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from "react";
 import './App.css';
+import { Route, Link, Router } from "react-router-dom";
 
-function App() {
+class App extends Component {
+render () {
   return (
     <div className="App">
       <header>
         <p>
           <h1>Virginia Beer and Brewery</h1>
         </p>
+        <nav>
+        </nav>
+        <main>
+          <Route path="/" exact component={Home}/>
+        </main>
      
       </header>
     </div>
   );
+}
 }
 
 export default App;
