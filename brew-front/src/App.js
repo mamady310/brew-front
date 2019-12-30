1d6d8fed2431a237c351e0e1f9b1eefdc4f99db6
Mamady Sillah <supurpisces310@msn.com>
2019-12-29 23:47:31 -0500

    Cleaned up app.js

---
+++ brew-front/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <p>
+          <h1>Virginia Beer and Brewery</h1>
+        </p>
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
