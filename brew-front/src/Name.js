893d69f0e18721d783dbfcc121f836d1a326e8f0
Mamady Sillah <supurpisces310@msn.com>
2019-12-31 11:15:24 -0500

    added middleware to react to see if I could get api fetch going

--- brew-front/src/Name.js
+++ brew-front/src/Name.js
@@ -3,10 +3,11 @@ import React from "react";
 class Name extends React.Component {
    state = {
       loading: true,
+      name: true,
    };
 
    async componentDidMount() {
-    const url = "http://localhost:5000/";
+    const url = "https://cors-anywhere.herokuapp.com/http://localhost:5000/";
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
